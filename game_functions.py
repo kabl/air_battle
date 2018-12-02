b3ff3210f20e8520ef5d3bfbfee078c33c586972
kabl <blaser_k@gmx.ch>
2018-12-02 18:28:38 +0100

    initial commit

---
+++ game_functions.py
@@ -0,0 +1,40 @@
+import sys
+import pygame
+from bullet import Bullet
+
+
+def check_events(ai_settings, screen, ship, bullets):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup(event, ship)
+
+
+def check_keydown(event, ai_settings, screen, ship, bullets):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
+def check_keyup(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+def update_screen(ai_settings, screen, ship, bullets):
+    screen.fill(ai_settings.bg_color)
+    for bullet in bullets:
+        bullet.draw_bullet()
+    ship.blitme()
+    pygame.display.flip()
+
+
