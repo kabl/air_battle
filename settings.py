7ef6304f5d8e21af1ae3cb3d8c3e6b93cf4cedb6
dev <4295394+kabl@users.noreply.github.com>
2026-03-15 12:17:09 +0100

    settings

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings():
 
     def __init__(self):
         self.screen_width = 1400
-        self.screen_height = 1050
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.air_plane_speed_factor = 3
         self.air_plane_speed_rotation = 2
